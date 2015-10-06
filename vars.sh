7927a1d452faea2790316a1e20872e3c36c9a49d
Abhinav Sarje <abhinav.sarje@gmail.com>
2015-10-05 17:38:58 -0700

    comments

--- vars.sh
+++ vars.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+## mandatory environmental variables
+
 export NETCDF_DIR=/usr
 export COMBBLAS_DIR=$HOME/sw/CombBLAS
 
 export PATOH_BIN=$HOME/sw/patoh/build/Linux-x86_64/patoh
+
+## optional environmental variables
 
 export BASE_DIR=$HOME/mpas/HAMPtOn.git
 export TEMP_DIR=/tmp/HAMPtOn
